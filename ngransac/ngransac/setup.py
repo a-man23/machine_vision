88912dd3b6cb0aea2d42b8407fbd41cda36a8a07
a-man23 <aman.patel0803@gmail.com>
2025-11-23 16:26:01 -0500

    Built C++ extension and got demo to run

--- ngransac/ngransac/setup.py
+++ ngransac/ngransac/setup.py
@@ -2,28 +2,28 @@ from setuptools import setup
 from torch.utils.cpp_extension import CppExtension, BuildExtension
 import os
 
-opencv_inc_dir = '' # directory containing OpenCV header files
-opencv_lib_dir = '' # directory containing OpenCV library files
+opencv_inc_dir = r'C:\Users\Aman.DESKTOP-VEB4T1B\AppData\Local\Programs\Python\Python312\Lib\site-packages\opencv\build\include' # directory containing OpenCV header files
+opencv_lib_dir = r'C:\Users\Aman.DESKTOP-VEB4T1B\AppData\Local\Programs\Python\Python312\Lib\site-packages\opencv\build\x64\vc16\lib' # directory containing OpenCV library files
 
 #if not explicitly provided, we try to locate OpenCV in the current Conda environment
-conda_env = os.environ['CONDA_PREFIX']
+# conda_env = os.environ['CONDA_PREFIX']
 
-if len(conda_env) > 0 and len(opencv_inc_dir) == 0 and len(opencv_lib_dir) == 0:
-	print("Detected active conda environment:", conda_env)
+# if len(conda_env) > 0 and len(opencv_inc_dir) == 0 and len(opencv_lib_dir) == 0:
+# 	print("Detected active conda environment:", conda_env)
 	
-	opencv_inc_dir = conda_env + '/include' 
-	opencv_lib_dir = conda_env + '/lib' 
+# 	opencv_inc_dir = conda_env + '/include' 
+# 	opencv_lib_dir = conda_env + '/lib' 
 
-	print("Assuming OpenCV dependencies in:")
-	print(opencv_inc_dir)
-	print(opencv_lib_dir)
+# 	print("Assuming OpenCV dependencies in:")
+# 	print(opencv_inc_dir)
+# 	print(opencv_lib_dir)
 
-if len(opencv_inc_dir) == 0:
-	print("Error: You have to provide an OpenCV include directory. Edit this file.")
-	exit()
-if len(opencv_lib_dir) == 0:
-	print("Error: You have to provide an OpenCV library directory. Edit this file.")
-	exit()
+# if len(opencv_inc_dir) == 0:
+# 	print("Error: You have to provide an OpenCV include directory. Edit this file.")
+# 	exit()
+# if len(opencv_lib_dir) == 0:
+# 	print("Error: You have to provide an OpenCV library directory. Edit this file.")
+# 	exit()
 
 setup(
 	name='ngransac',
@@ -32,7 +32,7 @@ setup(
 		sources=['ngransac.cpp','thread_rand.cpp'],
 		include_dirs=[opencv_inc_dir],
 		library_dirs=[opencv_lib_dir],
-		libraries=['opencv_core','opencv_calib3d'],
+		libraries=['opencv_world4120'],
 		extra_compile_args=['-fopenmp']
 		)],		
 	cmdclass={'build_ext': BuildExtension})
